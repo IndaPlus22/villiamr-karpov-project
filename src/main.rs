a39383c7b285fa8b24ba6135d21d697dce10d35a
Alexander karpov <karpov@kth.se>
2023-05-04 20:35:08 +0200

    Start on create_issue function

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
-
+use reqwest;
 fn main() {
-    let vars = vec!["INPUT_REPO", "INPUT_LATEST_PUSH", "INPUT_COMMITS", "INPUT_DIFF_URL", "INPUT_API_URL"];
+    let vars = vec!["INPUT_REPO", "INPUT_LATEST_PUSH", "INPUT_COMMITS", "INPUT_DIFF_URL", "INPUT_API_URL, INPUT_TOKEN"];
 
     for v in vars {
         match env::var(v) {
@@ -9,4 +9,12 @@ fn main() {
            Err(e)  => println!("Variable {} not set: {}", v ,e)
         }
     }
+}
+//Might be able to make asynchonous
+fn create_issue() {
+    let client = reqwest::Client::new();
+    let res = client.post()
+    .header(ACCEPT, "application/vnd.github.v3+json")
+    .header(AUTHORIZATION, "token ${{ secrets.GITHUB_TOKEN }}")
+
 }

f91f657d7bb703739a43f13d0c443d49bf8b0658
Villiam Riegler <ville@riegler.se>
2023-05-05 21:51:18 +0200

    slight cleaunup

--- src/main.rs
+++ src/main.rs
@@ -3,35 +3,34 @@ use serde_json::{json};
 use reqwest::{self, header::{HeaderMap, self}, Error};
 
 struct ApiInteraction {
-    token: String,
-    url: String
+    url: String,
+    headers: HeaderMap
 }
 
 impl ApiInteraction {
     fn new() -> ApiInteraction {
+        let mut header = HeaderMap::new();
+        header.insert(header::USER_AGENT, header::HeaderValue::from_static("TODO ACTION"));
+        header.insert(header::AUTHORIZATION,format!("token {}",std::env::var("INPUT_TOKEN").unwrap()).parse().unwrap());
+        header.insert(header::CONTENT_TYPE, header::HeaderValue::from_static("application/json"));
+
+
         return ApiInteraction{
-            token:std::env::var("INPUT_TOKEN").unwrap(), 
+            headers: header, 
             url:format!("{}/repos/{}/issues", std::env::var("INPUT_API_URL").unwrap(), std::env::var("INPUT_REPO").unwrap())
         };
     }
 
     async fn post_issue(self, title: String, body: String) -> Result<(), Error>{
-        let mut headers = HeaderMap::new();
-        headers.insert(header::USER_AGENT, header::HeaderValue::from_static("TODO ACTION"));
-        headers.insert(header::AUTHORIZATION,format!("token {}",self.token).parse().unwrap());
-        headers.insert(header::CONTENT_TYPE, header::HeaderValue::from_static("application/json"));
-
         let playload = json!({
             "title": title,
             "body": body,
         });
         
-        println!("{:?}",headers);
-
         let client = reqwest::Client::new();
 
         let resp = client.post(self.url)
-            .headers(headers)
+            .headers(self.headers)
             .json(&playload)
             .send()
             .await?;

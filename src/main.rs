5d01e01f979cbd0d093431bf9f796ad8701439a8
Alexander karpov <karpov@kth.se>
2023-05-05 14:33:31 +0200

    test

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,25 @@
 use reqwest;
-use serde_json::json;
+use std::error::Error;
 
-#[tokio::main]
-async fn main() -> Result<(), reqwest::Error> {
+fn main() -> Result<(), Box<dyn Error>> {
+
+    let token = std::env::var("INPUT_TOKEN")?;
     let title = "Found a bug";
     let body = "I'm having a problem with this.";
-    let token = "your-github-token-here";
-
-    let payload = json!({ "title": title, "body": body });
+    let payload = serde_json::json!({ "title": title, "body": body });
 
     let client = reqwest::Client::new();
     let res = client
         .post("https://api.github.com/repos/IndaPlus22/villiamr-karpov-project/issues")
         .header("Authorization", format!("token {}", token))
         .json(&payload)
-        .send()
-        .await?;
+        .send()?;
 
-    println!("Response status: {}", res.status());
+    if res.status().is_success() {
+        println!("Issue created successfully!");
+    } else {
+        println!("Failed to create issue: {}", res.status());
+    }
 
     Ok(())
 }

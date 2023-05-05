4a0b556ccdacedd13712c3a7e9c041d4638d29fe
Villiam Riegler <ville@riegler.se>
2023-05-05 18:35:40 +0200

    Testing api concat

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,21 @@
-use std::env;
+use std::{env, string};
 
-fn main() {
-    let token = std::env::var("INPUT_TOKEN");
-    println!("{:?}", token);
+struct ApiInteraction {
+    token: String,
+    url: String
+}
+
+impl ApiInteraction {
+    fn new(token_: String, baseurl: String, repo: String) -> ApiInteraction {
+        return ApiInteraction{token:token_, url:format!("{}{}/issues",baseurl, repo)};
+    }
+}
+
+fn main() {    
+    let api = ApiInteraction::new(
+        std::env::var("INPUT_TOKEN").unwrap(),
+        std::env::var("INPUT_API_URL").unwrap(), 
+        std::env::var("INPUT_REPO").unwrap());
+    
+    println!("{}",api.url);
 }

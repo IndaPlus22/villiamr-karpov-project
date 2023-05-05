783cac5536d7e8c95eed96e4cb52be7d6e7c4423
Alexander karpov <karpov@kth.se>
2023-05-05 14:10:45 +0200

    testing

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,25 @@
-use std::env;
 use reqwest;
+use std::error::Error;
 
-fn main() {
-    let vars = vec!["INPUT_REPO", "INPUT_LATEST_PUSH", "INPUT_COMMITS", "INPUT_DIFF_URL", "INPUT_API_URL"];
+fn main() -> Result<(), Box<dyn Error>> {
+    let token = std::env::var("INPUT_TOKEN")?;
 
-    for v in vars {
-        match env::var(v) {
-           Ok(val) => println!("{}: {}", v, val),
-           Err(e)  => println!("Variable {} not set: {}", v ,e)
-        }
-    }
-    let token = match env::var("INPUT_TOKEN") {
-        Ok(val) => val,
-        Err(e) => {
-            eprintln!("Error: {}", e);
-            std::process::exit(1);
-        }
-    };
-    match create_issue(&token).await {
-        Ok(()) => println!("Issue created successfully"),
-        Err(e) => eprintln!("Error creating issue: {}", e),
-    }
+    let title = "Found a bug";
+    let body = "I'm having a problem with this.";
+    let payload = serde_json::json!({ "title": title, "body": body });
 
-    Ok(())
-}
-
-//Might be able to make asynchonous
-async fn create_issue(token: &str) -> Result<(), reqwest::Error>{
-    let body = r#"{"title":"Found a bug","body":"I'm having a problem with this."}"#;
     let client = reqwest::Client::new();
     let res = client
-    .post("https://github.com/IndaPlus22/villiamr-karpov-project/issues")
-    .header("ACCEPT", "application/vnd.github.v3+json")
-    .header("AUTHORIZATION", format!("token {}", token))
-    .header("X-GITHUB-API-VERSION", "2022-11-28")
-    .body(body)
-    .send()
-    .await?;
-    //println!("Response status: {}", res.status());
-    let body1 = res.text().await?;
-    println!("Response body: {}", body);
+        .post("https://api.github.com/repos/IndaPlus22/villiamr-karpov-project/issues")
+        .header("Authorization", format!("token {}", token))
+        .json(&payload)
+        .send()?;
+
+    if res.status().is_success() {
+        println!("Issue created successfully!");
+    } else {
+        println!("Failed to create issue: {}", res.status());
+    }
+
     Ok(())
 }

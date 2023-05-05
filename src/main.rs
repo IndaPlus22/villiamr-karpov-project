3e8be93bb5fad4cc3ae8c35fcc4f4b72708b17d1
Alexander karpov <karpov@kth.se>
2023-05-05 12:43:32 +0200

    testing

--- src/main.rs
+++ src/main.rs
@@ -16,18 +16,17 @@ fn main() {
             std::process::exit(1);
         }
     };
-    //create_issue(&token);
+    create_issue(&token);
 }
 //Might be able to make asynchonous
 fn create_issue(token: &str) {
     let body = r#"{"title":"Found a bug","body":"I'm having a problem with this."}"#;
     let client = reqwest::Client::new();
-    let res = client.post()
-    .header(ACCEPT, "application/vnd.github.v3+json")
-    .header(AUTHORIZATION, "token ${{ secrets.GITHUB_TOKEN }}")
-    .header(X-GITHUB-API-VERSION, "2022-11-28")
+    let res = client.post("https://github.com/IndaPlus22/villiamr-karpov-project/issues")
+    .header("ACCEPT", "application/vnd.github.v3+json")
+    .header("AUTHORIZATION", "token ${{ secrets.GITHUB_TOKEN }}")
+    .header("X-GITHUB-API-VERSION", "2022-11-28")
     .body(body)
-    .send()
-    .await?;
+    .send();
     println!("Response status: {}", response.status());
 }

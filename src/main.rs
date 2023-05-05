b28acc26a4d6181a64bb1f3463b97107c80cfdfd
Villiam Riegler <ville@riegler.se>
2023-05-05 18:06:22 +0200

    Token test

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,6 @@
 use std::env;
 
 fn main() {
-    let vars = vec!["INPUT_REPO", "INPUT_LATEST_PUSH", "INPUT_COMMITS", "INPUT_DIFF_URL", "INPUT_API_URL"];
-
-    for v in vars {
-        match env::var(v) {
-           Ok(val) => println!("{}: {}", v, val),
-           Err(e)  => println!("Variable {} not set: {}", v ,e)
-        }
-    }
+    let token = std::env::var("INPUT_TOKEN");
+    println!("{:?}", token);
 }

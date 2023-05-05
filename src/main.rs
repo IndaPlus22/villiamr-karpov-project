19cf4c2c48ce5676fd0440267cdc99b46b8e4473
Villiam Riegler <ville@riegler.se>
2023-05-05 15:51:53 +0200

    Possible test fix?

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use reqwest;
+use reqwest::*;
 use std::error::Error;
 
 fn main(){
 
-    let token = std::env::var("INPUT_TOKEN")?;
+    let token = std::env::var("INPUT_TOKEN");
     let title = "Found a bug";
     let body = "I'm having a problem with this.";
     let payload = serde_json::json!({ "title": title, "body": body });
@@ -11,11 +11,12 @@ fn main(){
     let client = reqwest::blocking::Client::new();
     let res = client
         .post("https://api.github.com/repos/IndaPlus22/villiamr-karpov-project/issues")
-        .header("Authorization", format!("token {}", token))
+        .header("Authorization", format!("token {:?}", token))
         .json(&payload)
-        .send()?;
+        .send()
+        .unwrap();
 
     println!("Response status: {}", res.status());
-    let body = res.text()?;
-    println!("Response body:\n{}", body);
+    let body = res.text();
+    println!("Response body:\n{:?}", body);
 }
